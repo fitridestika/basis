20e4ce3523cd38d5254d4ffed7d402fbb8b2196f
sifa <sf7687138@gmail.com>
2024-09-24 12:10:26 +0700

    Konsep OOP

---
+++ src/main/kotlin/Data class D.kt
@@ -0,0 +1,4 @@
+fun main(){
+    val meri = Student("Meri", 20)
+    println(meri.toString())
+}
---
+++ src/main/kotlin/PersonC.kt
@@ -0,0 +1,6 @@
+class PersonC (var firstName: String, var lastName: String, var age: Int) {}
+fun main(){
+    val budi = PersonC("Budi", "Gunawan", 21)
+    println("Name : ${budi.firstName} ${budi.lastName}")
+    println("Age : ${budi.age}")
+}
